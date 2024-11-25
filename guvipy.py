5fe0972510bdd56738e7a96e2c0c54ae4443a47a
wasafairy <sskp22022@gmail.com>
2024-11-26 05:21:52 +0530

    Add files via upload

---
+++ guvipy.py
@@ -0,0 +1,13 @@
+class circle:
+    def __init__(self):
+        print("constructor is created")
+    def show_list(self,a):
+        print(a)
+a=[10,501,22,37,100,999,87,351]
+obj = circle()
+obj.show_list(a)
+
+
+        
+        
+    
